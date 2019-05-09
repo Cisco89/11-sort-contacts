a2efd892e2a4514715ff45b2b8f512004b9cd375
Francisco Adrian Garcia <franciscoadriangarcia1@gmail.com>
2019-05-09 00:40:51 -0600

    Update sort_contacts.py

--- sort_contacts.py
+++ sort_contacts.py
@@ -1,3 +1,5 @@
-def sort_contacts(param):
-    # code here
-    return ""
+def sort_contacts(dictionary):
+    sorted_list = []
+    for name,info in sorted(dictionary.items()):
+        sorted_list.append((name, info[0],info[1]))
+    return sorted_list
